74e3bcb84a79c879ab9e94f0e73723bd231d5654
drew <94761458+mountaindrewtech@users.noreply.github.com>
2022-09-29 10:01:43 -0400

    Add files via upload

---
+++ Intune-Pulseway Create a scheduled task.ps1
@@ -0,0 +1,5 @@
+﻿$action = (New-ScheduledTaskAction -Execute 'powershell.exe' "Stop-Process -Name Acrobat -Force"),
+    (New-ScheduledTaskAction -Execute 'Z:\obfuscated') 
+ $tr =  New-ScheduledTaskTrigger -Daily -At "11am"
+ $pr = New-ScheduledTaskPrincipal  -Groupid  "INTERACTIVE" 
+ Register-ScheduledTask  -TaskName "Task Name" -Description "Task Description" -Trigger $tr -Action $action -Principal $pr
