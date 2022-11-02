41291c4f1586c3137de6a8e0dc5605e233a7fa25
drew <94761458+mountaindrewtech@users.noreply.github.com>
2022-11-01 22:43:28 -0400

    Add files via upload
    no more disconnecting and remapping those users who save their creds when they have to change every 3 months

---
+++ Network Drive Connect Program/Form1.vb
@@ -0,0 +1,46 @@
+﻿'Title: EnterpriseData Reconnect Program
+'Author: Drew Schmidt
+'Date: 11/01/2022
+
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel
+
+Public Class Form1
+
+    'Run reconnect command on button click
+    Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+
+        Dim command = "/c net use z: /delete /y & cls & net use z: \\networkpath /user:domain\" + txtUsername.Text + " " + txtPassword.Text + " /p:yes || Timeout 5"
+
+        Process.Start("cmd.exe", command)
+
+        txtUsername.Text = String.Empty
+        txtPassword.Text = String.Empty
+
+    End Sub
+
+    'Run reconnect command on enter key - username textbox
+    Private Sub txtUsername_KeyDown(sender As Object, e As KeyEventArgs) Handles txtUsername.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            btnConnect_Click(Nothing, Nothing)
+        Else
+            Exit Sub
+        End If
+        e.SuppressKeyPress = True
+    End Sub
+
+    'Run reconnect command on enter key - password textbox
+    Private Sub txtPassword_KeyDown(sender As Object, e As KeyEventArgs) Handles txtPassword.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            btnConnect_Click(Nothing, Nothing)
+        Else
+            Exit Sub
+        End If
+        e.SuppressKeyPress = True
+    End Sub
+
+    'Close program on button click
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Close()
+    End Sub
+
+End Class
