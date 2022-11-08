0cba1990202d8dae4b3056ed6bfd2a837f9b74c9
drew <94761458+mountaindrewtech@users.noreply.github.com>
2022-11-08 11:13:40 -0500

    Add files via upload
    v1.1, removed debugging :)

---
+++ Network Drive Connect Program v1.1/Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.btnConnect = New System.Windows.Forms.Button()
+        Me.lblPassword = New System.Windows.Forms.Label()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.lblWatermark = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(239, 62)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 4
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'btnConnect
+        '
+        Me.btnConnect.Location = New System.Drawing.Point(158, 62)
+        Me.btnConnect.Name = "btnConnect"
+        Me.btnConnect.Size = New System.Drawing.Size(75, 23)
+        Me.btnConnect.TabIndex = 3
+        Me.btnConnect.Text = "Connect"
+        Me.btnConnect.UseVisualStyleBackColor = True
+        '
+        'lblPassword
+        '
+        Me.lblPassword.AutoSize = True
+        Me.lblPassword.Location = New System.Drawing.Point(12, 9)
+        Me.lblPassword.Name = "lblPassword"
+        Me.lblPassword.Size = New System.Drawing.Size(81, 15)
+        Me.lblPassword.TabIndex = 4
+        Me.lblPassword.Text = "PDI Password:"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(12, 27)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(8226)
+        Me.txtPassword.PlaceholderText = "Enter your PDI password"
+        Me.txtPassword.Size = New System.Drawing.Size(302, 23)
+        Me.txtPassword.TabIndex = 1
+        '
+        'lblWatermark
+        '
+        Me.lblWatermark.AutoSize = True
+        Me.lblWatermark.Location = New System.Drawing.Point(12, 66)
+        Me.lblWatermark.Name = "lblWatermark"
+        Me.lblWatermark.Size = New System.Drawing.Size(72, 15)
+        Me.lblWatermark.TabIndex = 6
+        Me.lblWatermark.Text = "WDP IT 2022"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(326, 90)
+        Me.Controls.Add(Me.lblWatermark)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.lblPassword)
+        Me.Controls.Add(Me.btnConnect)
+        Me.Controls.Add(Me.btnClose)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "WDP PDI EnterpriseData"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnClose As Button
+    Friend WithEvents btnConnect As Button
+    Friend WithEvents lblPassword As Label
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents lblWatermark As Label
+End Class
---
+++ Network Drive Connect Program v1.1/Form1.vb
@@ -0,0 +1,70 @@
+﻿'Title: EnterpriseData Reconnect Program
+'Version: 1.1.0
+'Author: Drew Schmidt
+'Date: 11/01/2022
+
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel
+
+Public Class Form1
+
+    'Run reconnect command on button click
+    Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+
+        ' If textbox is empty do not continue
+        If (txtPassword.Text = "") Then
+            MessageBox.Show("Please enter a password!")
+        Else
+            ' Start CMD as a process and run the reconnect command, enable standard error output
+            Dim cmdConnect As New Process()
+            Dim cntInfo As New ProcessStartInfo("cmd.exe", "/c net use z: /delete /y & cls & net use z: \\networkpath /user:C2050\%username% " + txtPassword.Text + " /p:yes")
+            cntInfo.UseShellExecute = False
+            cntInfo.CreateNoWindow = True
+            cntInfo.RedirectStandardError = True
+            cmdConnect.StartInfo = cntInfo
+            cmdConnect.Start()
+
+            ' Collect and output the standard errors
+            Dim cmdOutput As String
+            Using cmdStreamReader As System.IO.StreamReader = cmdConnect.StandardError
+                cmdOutput = cmdStreamReader.ReadToEnd()
+            End Using
+
+            ' Tell the user if the password is incorrect and clear the text box, otherwise open Z: and close
+            If cmdOutput.Contains("The specified network password is not correct.") Then
+                MessageBox.Show("Incorrect password!")
+                txtPassword.Text = String.Empty
+            Else
+                '' Process.Start("cmd.exe", "/c start Z:")
+                Dim cmdOpen As New Process()
+                Dim opnInfo As New ProcessStartInfo("cmd.exe", "/c start Z:")
+                opnInfo.UseShellExecute = False
+                opnInfo.CreateNoWindow = True
+                cmdOpen.StartInfo = opnInfo
+                cmdOpen.Start()
+                Close()
+            End If
+
+        End If
+
+    End Sub
+
+    'Run reconnect command on enter key while focused on textbox
+    Private Sub txtPassword_KeyUp(sender As Object, e As KeyEventArgs) Handles txtPassword.KeyUp
+
+        If e.KeyCode = Keys.Enter Then
+            btnConnect_Click(Nothing, Nothing)
+        Else
+            Exit Sub
+        End If
+        e.SuppressKeyPress = True
+
+    End Sub
+
+    'Close program on button click
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+
+        Close()
+
+    End Sub
+
+End Class
