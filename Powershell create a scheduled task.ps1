fbc7c368c39005bbbe8a102cf6e7f89e791201c0
drew <94761458+mountaindrewtech@users.noreply.github.com>
2022-12-29 21:37:59 -0500

    Update and rename Intune-Pulseway Create a scheduled task.ps1 to Powershell create a scheduled task.ps1

---
+++ Powershell create a scheduled task.ps1
@@ -0,0 +1,4 @@
+﻿$action = (New-ScheduledTaskAction -Execute 'executable' "argument") 
+ $tr =  New-ScheduledTaskTrigger -Daily -At "11am"
+ $pr = New-ScheduledTaskPrincipal  -Groupid  "INTERACTIVE" 
+ Register-ScheduledTask  -TaskName "Task Name" -Description "Task Description" -Trigger $tr -Action $action -Principal $pr
